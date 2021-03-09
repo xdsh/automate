f081d246458ffad376947f449e12972434ce956b
sHadex <47897796+xdsh@users.noreply.github.com>
2021-03-09 17:00:30 +0300

    Update cloud9_auto.sh

--- cloud9_auto.sh
+++ cloud9_auto.sh
@@ -11,3 +11,11 @@ scripts/install-sdk.sh
 
 node server.js
 #Now visit http://localhost:8181/ide.html to load Cloud9.
+--------------------------------------------------------------------
+#Success!
+#run 'node server.js -p 8080 -a :' to launch Cloud9
+#Starting standalone
+#Connect server listening at http://127.0.0.1:8181
+#CDN: version standalone initialized /home/nodejs/c9sdk/build
+#Started '/home/nodejs/c9sdk/configs/standalone' with config 'standalone'!
+#Cloud9 is up and running
